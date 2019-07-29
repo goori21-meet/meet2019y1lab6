5369e92d7312d7ab535b164be1aad25896a11a73
goori21-meet <goori21@meet.mit.edu>
2019-07-29 02:01:35 -0400

    Stopping here

--- lab_6.py
+++ lab_6.py
@@ -1,6 +1,18 @@
-result=[]
-for count in range(1,100):
-    if count % 3==0:
-        result.append("fizz")
-    else:
-        result.append(count)
+def add_numbers(num_list):
+    num_list2=0
+
+    for num in num_list:
+
+        print(num)
+        num_list2=num_list2+num
+    return num_list2
+
+
+    
+    
+    
+
+
+
+answer = add_numbers(range(1,10+1))
+print(answer)
